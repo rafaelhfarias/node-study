3e72fa89b8d07095c44ffcab85ba6cb99de24966
Rafael Hipólito <rhf@Laptop-de-Rafael.local>
2023-03-14 11:06:58 -0300

    remove unused files

--- databases/src/strategies/postgres.js
+++ databases/src/strategies/postgres.js
@@ -1,10 +1,61 @@
 import { ICrud } from "./interfaces/interfaceCrud.js"
+import Sequelize from "sequelize";
 
-export class Postgres extends ICrud{
-    constructor(){
+export class Postgres extends ICrud {
+    constructor() {
         super()
+        this._driver = null
+        this._heroes = null
+        this._connect()
     }
-    create(item){
+
+    _connect() {
+        this._driver = new Sequelize.Sequelize('heroes', 'rafaelhf', 'password',
+            {
+                host: 'localhost',
+                dialect: 'postgres',
+                quoteIdentifiers: false,
+                operatorsAliases: false
+            }
+        )
+    }
+
+    async isConnected() {
+        try {
+            await this._driver.authenticate()
+            return true
+        } catch (error) {
+            console.log('failed to connect', error)
+            return false
+        }
+    }
+
+    async defineModel() {
+        this._heroes = driver.define('heroes', {
+            id: {
+                type: Sequelize.INTEGER,
+                required: true,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            name: {
+                type: Sequelize.STRING,
+                required: true
+            },
+            power: {
+                type: Sequelize.STRING,
+                required: true
+            }
+        }, {
+            tableName: 'TB_HEROES',
+            freezeTableName: false,
+            timestamps: false
+        }
+        )
+        await Heroes.sync()
+    }
+
+    create(item) {
         console.log("Item foi salvo em Postgres")
     }
 }
